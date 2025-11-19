e4ed741b3617bd7889c7d639c991e12ab5fe6197
minhtrien29 <minhtrien3103@gmail.com>
2025-11-19 16:23:49 +0700

    Update Ball.cpp

--- GamPenalty/Ball.cpp
+++ GamPenalty/Ball.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿// File: ball.cpp
+#include "ball.h"
+#include "constants.h" // Cần để lấy SCREEN_W, SCREEN_H
+
+Ball::Ball() {
+    Reset();
+    radius = 18.0f;
+    moving = false;
+}
+
+void Ball::Reset() {
+    pos = { SCREEN_W / 2.0f, SCREEN_H - 150.0f };
+    vel = { 0,0 };
+    moving = false;
+}
