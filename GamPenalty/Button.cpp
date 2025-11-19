ca62bfb5f42a92870ae2a65340c7b2856361a9e5
minhtrien29 <minhtrien3103@gmail.com>
2025-11-19 23:10:09 +0700

    Update Button.cpp

--- GamPenalty/Button.cpp
+++ GamPenalty/Button.cpp
@@ -1,1 +1,11 @@
+#include "button.h"
+#include "raylib.h"
 
+void DrawButton(const Button& b) {
+    DrawRectangleRec(b.rect, b.color);
+    DrawRectangleLines((int)b.rect.x, (int)b.rect.y, (int)b.rect.width, (int)b.rect.height, Fade(BLACK, 0.2f));
+    DrawText(b.text,
+        (int)(b.rect.x + (b.rect.width - MeasureText(b.text, 20)) / 2),
+        (int)(b.rect.y + (b.rect.height - 20) / 2),
+        20, WHITE);
+}
