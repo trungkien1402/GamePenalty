152f8da03a16601d8bf604e45b2a142c8405916c
minhtrien29 <minhtrien3103@gmail.com>
2025-11-19 22:50:41 +0700

    Update Sound.cpp

--- GamPenalty/Sound.cpp
+++ GamPenalty/Sound.cpp
@@ -1,10 +1,8 @@
-﻿#include "sound.h"
+#include "sound.h"
 #include "raylib.h"
 
-void SoundManager::LoadMusic(const char* path)
-{
+void SoundManager::LoadMusic(const char* path) {
     music = LoadMusicStream(path);
-
     if (music.ctxData != NULL) {
         musicLoaded = true;
         music.looping = true;
@@ -14,30 +12,14 @@ void SoundManager::LoadMusic(const char* path)
     }
 }
 
-void SoundManager::PlayMusic()
-{
-    if (musicLoaded)
-        PlayMusicStream(music);
-}
-
-void SoundManager::UpdateMusic()
-{
-    if (musicLoaded)
-        UpdateMusicStream(music);
-}
-
-void SoundManager::ToggleMusic()
-{
+void SoundManager::PlayMusic() { if (musicLoaded) PlayMusicStream(music); }
+void SoundManager::UpdateMusic() { if (musicLoaded) UpdateMusicStream(music); }
+void SoundManager::ToggleMusic() {
     if (!musicLoaded) return;
-
-    if (IsMusicStreamPlaying(music))
-        PauseMusicStream(music);
-    else
-        ResumeMusicStream(music);
+    if (IsMusicStreamPlaying(music)) PauseMusicStream(music);
+    else ResumeMusicStream(music);
 }
-
-bool SoundManager::IsMusicPlaying()
-{
+bool SoundManager::IsMusicPlaying() {
     if (!musicLoaded) return false;
     return IsMusicStreamPlaying(music);
 }
