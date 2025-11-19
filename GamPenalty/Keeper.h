8a6d3129df96fb7eb6c3387f6fc08415eea61a1f
minhtrien29 <minhtrien3103@gmail.com>
2025-11-19 22:48:14 +0700

    Update Keeper.h

--- GamPenalty/Keeper.h
+++ GamPenalty/Keeper.h
@@ -1,15 +1,13 @@
-﻿// File: keeper.h (Cập nhật)
 #pragma once
 #include "raylib.h"
 
 struct Keeper {
     Rectangle rect;
     float speed;
-    int dir; // -1: Trái, 0: Giữa, 1: Phải
+    int dir; // -1: Trái, 0: Đứng, 1: Phải
     float minX, maxX;
 
-    // SỬA: Thêm "float goalH" để biết chiều cao gôn
+    Keeper();
     void Reset(float goalX, float goalW, float goalY, float goalH);
     void Update(float dt, bool ballMoving);
-    Keeper();
 };
