81ff943c1902f3f7e8feefce79c34e27a1df87ae
quanhoang-gon <quanbui0208@gmail.com>
2025-11-20 20:22:35 +0700

    Update GameState.h

--- GamPenalty/GameState.h
+++ GamPenalty/GameState.h
@@ -1,9 +1,11 @@
 #pragma once
-// File này dùng chung cho cả Menu và Game để hiểu nhau
+// File này chứa enum trạng thái game và được dùng chung giữa Menu và Game
+
+// Liệt kê các trạng thái chính của trò chơi
 enum GameState {
-    STATE_MENU,
-    STATE_INSTRUCTIONS,
-    STATE_SOUND,
-    STATE_GAME,
-    STATE_EXIT
+    STATE_MENU,          // Trạng thái menu chính
+    STATE_INSTRUCTIONS,  // Trạng thái xem hướng dẫn
+    STATE_SOUND,         // Trạng thái chỉnh âm thanh
+    STATE_GAME,          // Trạng thái đang chơi game
+    STATE_EXIT           // Thoát game
 };
