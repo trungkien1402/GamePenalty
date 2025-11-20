ca8ec244a2da34fecf8d30dcbd033fbce09197d9
2uanx <tkxike@gmail.com>
2025-11-20 15:42:37 +0700

    Update Sound.h

--- GamPenalty/Sound.h
+++ GamPenalty/Sound.h
@@ -6,10 +6,25 @@ private:
     Music music;
     bool musicLoaded = false;
 
+    // Khởi tạo các biến Sound ngay trong khai báo (Để khắc phục C26495)
+    Sound goalSFX = { 0 };
+    Sound missSFX = { 0 };
+
 public:
+    // [KHAI BÁO CONSTRUCTOR] Khắc phục lỗi thiếu hàm tạo
+    SoundManager();
+
     void LoadMusic(const char* path);
-    void PlayMusic();
+
+    // Hàm UpdateMusic cần được đặt ở đây
     void UpdateMusic();
+
+    void PlayMusic(); // Khắc phục lỗi "PlayMusic không tồn tại"
     void ToggleMusic();
     bool IsMusicPlaying();
+
+    void LoadSFX(const char* goalPath, const char* missPath);
+    void PlayGoalSFX();
+    void PlayMissSFX();
+    bool IsSFXPlaying();
 };
