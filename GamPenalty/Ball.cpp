29e812ea4683efb698f0385203716dbf211806b9
minhtrien29 <minhtrien3103@gmail.com>
2025-11-20 17:34:54 +0700

    Update Ball.cpp

--- GamPenalty/Ball.cpp
+++ GamPenalty/Ball.cpp
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿#include "ball.h"
-=======
 #include "ball.h"
->>>>>>> 9b706097fc97aff184be64f9d710b8c7030e988f
 #include "constants.h"
 
 Ball::Ball() {
@@ -16,8 +12,4 @@ void Ball::Reset() {
     pos = { SCREEN_W / 2.0f, SCREEN_H - 150.0f };
     vel = { 0,0 };
     moving = false;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9b706097fc97aff184be64f9d710b8c7030e988f
